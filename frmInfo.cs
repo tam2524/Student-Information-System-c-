92a156174106976acb71f9175719ff157cc8af11
MatCodings <128329980+tam2524@users.noreply.github.com>
2023-10-20 16:27:46 +0800

    Add files via upload

---
+++ frmInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace StudentInformationSystem
+{
+    public partial class studentInfo : Form
+    {
+        public studentInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void studentInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
